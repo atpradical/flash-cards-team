c17e08cb15447123ba979291a35c6bf84d037749
Ivan Dolgikh <ivan.dolgikh1199@gmail.com>
2024-05-28 00:23:29 +0300

    feature: tabSwitcher component && styles && storybook

---
+++ src/components/ui/tabSwitcher/tabSwitcher.stories.tsx
@@ -0,0 +1,56 @@
+import type { Meta, StoryObj } from '@storybook/react'
+
+import { TabSwitcher, TabType } from '@/components/ui/tabSwitcher/tabSwitcher'
+import { action } from '@storybook/addon-actions'
+
+const mockTabs1: TabType[] = [
+  { title: 'Tab title 1', value: 'tab-value-1' },
+  { title: 'Tab title 2', value: 'tab-value-2' },
+  { title: 'Tab title 3', value: 'tab-value-3' },
+]
+
+const mockTabs2: TabType[] = [
+  { disabled: true, title: 'Tab title 1', value: 'tab-value-1' },
+  { disabled: true, title: 'Tab title 2', value: 'tab-value-2' },
+  { title: 'Tab title 3', value: 'tab-value-3' },
+  { title: 'Tab title 4', value: 'tab-value-4' },
+]
+
+const meta = {
+  argTypes: {},
+  component: TabSwitcher,
+  tags: ['autodocs'],
+  title: 'Components/TabSwitcher',
+} satisfies Meta<typeof TabSwitcher>
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+export const TabSwitcherWithLabel: Story = {
+  args: {
+    label: 'Tab Switcher Label',
+    onValueChange: action('Mock action invoked'),
+    tabs: mockTabs1,
+  },
+}
+export const BaseTabSwitcher: Story = {
+  args: {
+    onValueChange: action('Mock action invoked'),
+    tabs: mockTabs1,
+  },
+}
+
+export const TabSwitcherWithDefaultValue: Story = {
+  args: {
+    defaultValue: 'tabSwitcher-value-2',
+    onValueChange: action('Mock action invoked'),
+    tabs: mockTabs1,
+  },
+}
+
+export const TabSwitcherWithSeveralDisabledTabs: Story = {
+  args: {
+    onValueChange: action('Mock action invoked'),
+    tabs: mockTabs2,
+  },
+}
---
+++ src/components/ui/tabSwitcher/tabSwitcher.tsx
@@ -0,0 +1,60 @@
+import { ComponentPropsWithoutRef } from 'react'
+
+import * as Tabs from '@radix-ui/react-tabs'
+import clsx from 'clsx'
+
+import s from './tabSwitcher.module.scss'
+
+import { Typography } from '../typography'
+
+export type TabType = {
+  disabled?: boolean
+  title: string
+  value: string
+}
+
+type TabProps = {
+  label?: string
+  tabs: TabType[]
+} & ComponentPropsWithoutRef<typeof Tabs.Root>
+
+export const TabSwitcher = ({ className, defaultValue, label, tabs, ...rest }: TabProps) => {
+  const classNames = {
+    label: clsx(s.label),
+    list: clsx(s.tabsList),
+    root: clsx(s.tabsRoot, className),
+    tabTitle: clsx(s.tabTitle),
+    trigger: clsx(s.tabsTrigger),
+  }
+
+  const firstNotDisabledTab = tabs.find(tab => !tab.disabled)
+
+  return (
+    <Tabs.Root
+      activationMode={'automatic'}
+      className={classNames.root}
+      defaultValue={defaultValue ?? firstNotDisabledTab!.value ?? tabs[0].value}
+      {...rest}
+    >
+      {label && (
+        <Typography className={classNames.label} variant={'body2'}>
+          {label}
+        </Typography>
+      )}
+      <Tabs.List className={classNames.list}>
+        {tabs.map(tab => (
+          <Tabs.Trigger
+            className={classNames.trigger}
+            disabled={tab.disabled}
+            key={tab.value}
+            value={tab.value}
+          >
+            <Typography className={classNames.tabTitle} variant={'body1'}>
+              {tab.title}
+            </Typography>
+          </Tabs.Trigger>
+        ))}
+      </Tabs.List>
+    </Tabs.Root>
+  )
+}
