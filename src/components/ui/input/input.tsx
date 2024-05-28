fec6347aa497f21fed957db4285fde44843c32a9
eleonora <nebrudko18@gmail.com>
2024-05-28 20:49:06 +0300

    added stories, some styles

--- src/components/ui/input/input.tsx
+++ src/components/ui/input/input.tsx
@@ -1,34 +1,41 @@
-import { ComponentPropsWithoutRef, ElementType } from 'react'
+import { ComponentPropsWithoutRef, ElementType, ReactNode } from 'react'
 import clsx from 'clsx'
 import s from './input.module.scss'
 import { Typography } from '../typography'
 
 type InputVariant = 'active' | 'default' | 'disabled' | 'error' | 'focus' | 'hover'
 
+export type Icon = {
+  type: 'eye' | 'search'
+  icon: ReactNode
+}
+
 type Props<T extends ElementType> = {
   className?: string
+  icons: Icon[]
   placeholder?: string
   helperText?: string
   label?: string
-  labelPosition?: 'left' | 'top'
+  labelPosition?: 'top'
   size?: 'large' | 'medium' | 'small'
   variant: InputVariant
 } & ComponentPropsWithoutRef<T>
 
 export const Input = <T extends ElementType = 'input'>({
   className,
-  label,
+  label = 'Label',
   helperText,
-  placeholder = 'input',
-  labelPosition = 'top',
-  size,
-  variant,
+  placeholder = 'Enter here...',
+  size = 'medium',
+  icons,
+  variant = 'default',
   ...rest
 }: Props<T>) => {
   const inputId = `input-${Math.random().toString(36).slice(2, 11)}`
 
   const inputElement = (
     <input
+      placeholder={placeholder}
       id={inputId}
       className={clsx(s.input, s[`input-${variant}`], size && s[`input-${size}`], className)}
       {...rest}
@@ -41,17 +48,20 @@ export const Input = <T extends ElementType = 'input'>({
     </label>
   )
 
+  const renderIcons = () => {
+    return icons?.map((iconData, index) => (
+      <button key={index} className={clsx(s.icon, s[`icon-${iconData.type}`])}>
+        {iconData.icon}
+      </button>
+    ))
+  }
+
   return (
     <div className={s.inputWrapper}>
-      {label && labelPosition === 'top' && labelElement}
-      {labelPosition === 'left' ? (
-        <div className={s.horizontalLayout}>
-          {labelElement}
-          {inputElement}
-        </div>
-      ) : (
-        inputElement
-      )}
+      {labelElement}
+      {inputElement}
+      {renderIcons()}
+
       {helperText && <Typography variant="error">{helperText}</Typography>}
     </div>
   )
