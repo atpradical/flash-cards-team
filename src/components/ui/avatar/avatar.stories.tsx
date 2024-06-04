554b8a1f7125e2135d4281a36a3e0b55dc1f9bdf
Ivan Dolgikh <ivan.dolgikh1199@gmail.com>
2024-06-04 13:51:57 +0300

    feature: avatar component & styles & story

---
+++ src/components/ui/avatar/avatar.stories.tsx
@@ -0,0 +1,66 @@
+import type { Meta, StoryObj } from '@storybook/react'
+
+import avatarDefault from '@/assets/webp/avatar-default.webp'
+import { Avatar } from '@/components/ui/avatar/avatar'
+
+const meta = {
+  argTypes: {
+    size: {
+      control: 'inline-radio',
+      options: ['s', 'm', 'l'],
+    },
+  },
+  component: Avatar,
+  tags: ['autodocs'],
+  title: 'Components/Avatar',
+} satisfies Meta<typeof Avatar>
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+export const All: Story = {
+  render: () => <AllAvatars />,
+}
+
+export const Small: Story = {
+  args: {
+    size: 's',
+    src: avatarDefault,
+    title: 'JD',
+  },
+}
+
+export const Medium: Story = {
+  args: {
+    size: 'm',
+    src: avatarDefault,
+    title: 'JD',
+  },
+}
+
+export const Large: Story = {
+  args: {
+    size: 'm',
+    src: avatarDefault,
+    title: 'JD',
+  },
+}
+
+export const Fallback: Story = {
+  args: {
+    title: 'JD',
+  },
+}
+
+function AllAvatars() {
+  return (
+    <div style={{ display: 'flex', gap: '1rem' }}>
+      <Avatar size={'s'} title={'JD'} />
+      <Avatar size={'s'} src={avatarDefault} title={'JD'} />
+      <Avatar title={'JD'} />
+      <Avatar src={avatarDefault} title={'JD'} />
+      <Avatar size={'l'} title={'JD'} />
+      <Avatar size={'l'} src={avatarDefault} title={'JD'} />
+    </div>
+  )
+}
