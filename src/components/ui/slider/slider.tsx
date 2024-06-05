3cfb870c3cfaf1bf11c5420b2dfc3bbcb9bfe066
eleonora <nebrudko18@gmail.com>
2024-06-05 12:36:38 +0300

    fixed slider&&styles&&partly

--- src/components/ui/slider/slider.tsx
+++ src/components/ui/slider/slider.tsx
@@ -1,3 +1,5 @@
+import { ComponentPropsWithoutRef } from 'react'
+
 import * as RadixSlider from '@radix-ui/react-slider'
 
 import s from './slider.module.scss'
@@ -5,13 +7,11 @@ import s from './slider.module.scss'
 import { Typography } from '../typography'
 
 type Props = {
-  defaultValue?: number[]
   max?: number
   min?: number
-  minStepsBetweenThumbs?: number
   onValueChange: (value: number[]) => void
   value?: number[]
-}
+} & ComponentPropsWithoutRef<typeof RadixSlider.Root>
 
 export const Slider = (props: Props) => {
   const {
@@ -25,16 +25,16 @@ export const Slider = (props: Props) => {
   } = props
 
   return (
-    <form className={s.sliderForm}>
+    <div className={s.sliderForm}>
       <Typography className={s.outputWrap} variant={'body1'}>
-        {value ? value[0].toFixed(0) : null}
+        {value && value[0].toFixed(0)}
       </Typography>
       <RadixSlider.Root
         className={s.sliderRoot}
         defaultValue={defaultValue}
         max={max ? max : undefined}
         min={min}
-        minStepsBetweenThumbs={1}
+        minStepsBetweenThumbs={minStepsBetweenThumbs}
         onValueChange={onValueChange}
         step={1}
         value={value}
@@ -47,8 +47,8 @@ export const Slider = (props: Props) => {
         <RadixSlider.Thumb aria-label={'End Thumb'} className={s.sliderThumb} />
       </RadixSlider.Root>
       <Typography className={s.outputWrap} variant={'body1'}>
-        {value ? value[1].toFixed(0) : null}
+        {value && value[1].toFixed(0)}
       </Typography>
-    </form>
+    </div>
   )
 }
