8793411a9d83f7780c166ea70a84cce13ba35818
Ivan Dolgikh <ivan.dolgikh1199@gmail.com>
2024-06-10 19:10:50 +0300

    feature: Layout component: Header / Page / FlexContainer & styles & storybook

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,26 @@
 import { Button } from '@/components/ui/button'
+import { FlexContainer } from '@/shared/ui/flex-container'
+import { Layout } from '@/shared/ui/layout'
+
+import { Header } from './shared/ui/header'
+import { Page } from './shared/ui/page'
 
 export function App() {
   return (
-    <>
-      <div>Hi Team 🤘🤘🤘</div>
-      <div>
-        <Button>Primary</Button>
-        <Button variant={'secondary'}>Secondary</Button>
-        <Button as={'a'} variant={'link'}>
-          Button as link
-        </Button>
-      </div>
-    </>
+    <Layout>
+      <Header />
+      <Page>
+        <FlexContainer>
+          <div>Hi Team 🤘🤘🤘</div>
+          <div>
+            <Button>Primary</Button>
+            <Button variant={'secondary'}>Secondary</Button>
+            <Button as={'a'} variant={'link'}>
+              Button as link
+            </Button>
+          </div>
+        </FlexContainer>
+      </Page>
+    </Layout>
   )
 }
---
+++ src/shared/ui/header/header.stories.tsx
@@ -0,0 +1,33 @@
+import type { Meta, StoryObj } from '@storybook/react'
+
+import Logo from '@/assets/components/svgIcons/Logo'
+import avatarDefault from '@/assets/webp/avatar-default.webp'
+import { Avatar } from '@/components/ui/avatar'
+import { FlexContainer } from '@/shared/ui/flex-container'
+
+import { Header } from './header'
+
+const meta = {
+  argTypes: {},
+  component: Header,
+  tags: ['autodocs'],
+  title: 'Layout/Header',
+} satisfies Meta<typeof Header>
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+export const HeaderEmpty: Story = {
+  render: () => <Header />,
+}
+
+export const HeaderWithLogoAndAvatar: Story = {
+  render: () => (
+    <Header>
+      <FlexContainer jc={'space-between'}>
+        <Logo />
+        <Avatar size={'s'} src={avatarDefault} title={'JD'} />
+      </FlexContainer>
+    </Header>
+  ),
+}
