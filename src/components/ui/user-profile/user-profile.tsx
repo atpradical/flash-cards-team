4064a1aa893070bfe700efea4172893d50201209
M4k4v3ll1 <141486338+M4k4v3ll1@users.noreply.github.com>
2024-06-17 16:44:15 +0400

    fix: UserProfile is wrapped in FlexContainer, extra style is removed

--- src/components/ui/user-profile/user-profile.tsx
+++ src/components/ui/user-profile/user-profile.tsx
@@ -1,6 +1,7 @@
 import { Avatar } from '@/components/ui/avatar'
 import { Button } from '@/components/ui/button'
 import { Typography } from '@/components/ui/typography'
+import { FlexContainer } from '@/shared/ui/flex-container'
 import clsx from 'clsx'
 
 import s from './user-profile.module.scss'
@@ -11,22 +12,20 @@ type Props = {
   userName?: string
 }
 export const UserProfile = ({ isAuthorized, src, userName }: Props) => {
-  const cn = { container: clsx(s.container), link: clsx(s.link, src) }
+  const cn = { link: clsx(s.link) }
 
   return (
-    <>
+    <FlexContainer gap={'14px'} jc={'end'}>
       {isAuthorized ? (
-        <div className={cn.container}>
+        <>
           <Button className={cn.link} variant={'link'}>
             <Typography variant={'subtitle1'}>{userName}</Typography>
           </Button>
           <Avatar size={'s'} src={src} title={'Photo'} />
-        </div>
+        </>
       ) : (
-        <div>
-          <Button variant={'secondary'}>Sign In</Button>
-        </div>
+        <Button variant={'secondary'}>Sign In</Button>
       )}
-    </>
+    </FlexContainer>
   )
 }
