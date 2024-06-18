52e39aa61fcf21f2a4bbb454c1a96222d46e6a77
M4k4v3ll1 <141486338+M4k4v3ll1@users.noreply.github.com>
2024-06-18 09:49:30 +0400

    feature: add controlled-radio

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,41 @@
+import { useForm } from 'react-hook-form'
+
 import { Button } from '@/components/ui/button'
 import { FlexContainer } from '@/shared/ui/flex-container'
+import { ControlledRadio } from '@/shared/ui/form-components/controlled-radio'
+import { Header } from '@/shared/ui/header'
 import { Layout } from '@/shared/ui/layout'
+import { Page } from '@/shared/ui/page'
+import { Option } from '@/types'
 
-import { Header } from './shared/ui/header'
-import { Page } from './shared/ui/page'
+type SomeFormValues = {
+  someRadio: string
+}
 
 export function App() {
+  const { control, handleSubmit } = useForm<SomeFormValues>()
+
+  const foo = handleSubmit(data => {
+    console.log(data)
+  })
+  const mockRadioOptions: Option[] = [
+    { id: '1', label: 'Default', value: '1' },
+    { id: '2', label: 'Comfortable', value: '2' },
+    { id: '3', label: 'Luxury', value: '3' },
+    { id: '4', label: 'Ultra wealth', value: '4' },
+  ]
+
   return (
     <Layout>
       <Header />
       <Page>
-        <FlexContainer>
-          <div>Hi Team 🤘🤘🤘</div>
-          <div>
-            <Button>Primary</Button>
-            <Button variant={'secondary'}>Secondary</Button>
-            <Button as={'a'} variant={'link'}>
-              Button as link
-            </Button>
-          </div>
-        </FlexContainer>
+        <form onSubmit={foo}>
+          <FlexContainer ai={'start'} fd={'column'} gap={'10px'} style={{ maxWidth: '500px' }}>
+            <div>Hi Team 🤘🤘🤘 Controlled components examples:</div>
+            <ControlledRadio control={control} name={'someRadio'} options={mockRadioOptions} />
+            <Button>Submit</Button>
+          </FlexContainer>
+        </form>
       </Page>
     </Layout>
   )
