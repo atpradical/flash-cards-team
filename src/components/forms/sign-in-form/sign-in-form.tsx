4b09b19cb3df328a5fab49b138ce3dd72171745e
eleonora <nebrudko18@gmail.com>
2024-06-19 13:44:53 +0300

    fixed: code reorganization

--- src/components/forms/sign-in-form/sign-in-form.tsx
+++ src/components/forms/sign-in-form/sign-in-form.tsx
@@ -3,7 +3,7 @@ import { useForm } from 'react-hook-form'
 import { Button } from '@/components/ui/button'
 import { Card } from '@/components/ui/card'
 import { Typography } from '@/components/ui/typography'
-import { emailSchema, passwordSchema } from '@/shared/schemes'
+import { emailSchema, passwordSchema, rememberMeSchema } from '@/shared/schemes'
 import { FlexContainer } from '@/shared/ui/flex-container'
 import { ControlledCheckbox } from '@/shared/ui/form-components/controlled-checkbox'
 import { ControlledTextField } from '@/shared/ui/form-components/controlled-text-field'
@@ -17,13 +17,13 @@ type SignInFormProps = {
   onSubmit: (data: FormValues) => void
 }
 
-const SignInSchema = z.object({
+const SignInFormSchema = z.object({
   email: emailSchema,
   password: passwordSchema,
-  rememberMe: z.boolean().optional(),
+  rememberMe: rememberMeSchema,
 })
 
-type FormValues = z.infer<typeof SignInSchema>
+type FormValues = z.infer<typeof SignInFormSchema>
 
 export const SignInForm = ({ onSubmit }: SignInFormProps) => {
   const { control, handleSubmit } = useForm<FormValues>({
@@ -33,7 +33,7 @@ export const SignInForm = ({ onSubmit }: SignInFormProps) => {
       rememberMe: false,
     },
     mode: 'onSubmit',
-    resolver: zodResolver(SignInSchema),
+    resolver: zodResolver(SignInFormSchema),
   })
 
   const formHandler = handleSubmit(data => {
@@ -42,12 +42,9 @@ export const SignInForm = ({ onSubmit }: SignInFormProps) => {
 
   const cn = {
     buttonSign: clsx(s.buttonSign),
-    checkbox: clsx(s.checkbox),
     container: clsx(s.container),
     form: clsx(s.form),
     link: clsx(s.link),
-    textField: clsx(s.textField),
-    typography: clsx(s.typography),
   }
 
   return (
@@ -71,23 +68,18 @@ export const SignInForm = ({ onSubmit }: SignInFormProps) => {
               placeholder={'Enter your password'}
               variant={'password'}
             />
-
             <ControlledCheckbox control={control} label={'Remember me'} name={'rememberMe'} />
           </FlexContainer>
-        </form>
-        <FlexContainer jc={'flex-end'}>
-          <Button className={cn.link} variant={'link'}>
-            <Typography as={'a'} variant={'body2'}>
+          <FlexContainer ai={'center'} jc={'flex-end'}>
+            <Typography as={'a'} className={cn.link} variant={'body2'}>
               Forgot Password?
             </Typography>
+          </FlexContainer>
+          <Button fullWidth variant={'primary'}>
+            Sign In
           </Button>
-        </FlexContainer>
-        <Button fullWidth variant={'primary'}>
-          Sign In
-        </Button>
-        <Typography className={cn.typography} variant={'body2'}>
-          Don&apos;t have an account?
-        </Typography>
+        </form>
+        <Typography variant={'body2'}>Don&apos;t have an account?</Typography>
         <Button className={cn.buttonSign} variant={'link'}>
           Sign Up
         </Button>
