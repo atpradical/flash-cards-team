1d00fb7445127f1caa3ef5fe12c80f626bb2d37d
Ivan Trandin <trandinii@gmail.com>
2024-06-20 11:27:29 +0400

    init: selfRateForm with styles and story

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
-import { ForgotPasswordForm } from '@/components/ui/forms/forgot-password-form'
-import { SignUpForm } from '@/components/ui/forms/sign-up-form'
+import { ForgotPasswordForm } from '@/components/forms/forgot-password-form'
+import { SignUpForm } from '@/components/forms/sign-up-form'
 import { FlexContainer } from '@/shared/ui/flex-container'
 import { Header } from '@/shared/ui/header'
 import { Layout } from '@/shared/ui/layout'
--- src/components/forms/self-rate-form/self-rate-form.stories.tsx
+++ src/components/forms/self-rate-form/self-rate-form.stories.tsx
@@ -1,18 +1,18 @@
-import { ForgotPasswordForm } from '@/components/ui/forms/forgot-password-form/forgotPasswordForm'
+import { SelfRateForm } from '@/components/forms/self-rate-form/self-rate-form'
 import { action } from '@storybook/addon-actions'
 import { Meta, StoryObj } from '@storybook/react'
 
 const meta = {
   argTypes: {},
-  component: ForgotPasswordForm,
+  component: SelfRateForm,
   tags: ['autodocs'],
-  title: 'Forms/ForgotPasswordForm',
-} satisfies Meta<typeof ForgotPasswordForm>
+  title: 'Forms/SelfRateForm',
+} satisfies Meta<typeof SelfRateForm>
 
 export default meta
 type Story = StoryObj<typeof meta>
 
-export const ForgotPasswordBase: Story = {
+export const SelfRateFormBase: Story = {
   args: {
     onSubmit: action('onSubmit action invoked!'),
   },
