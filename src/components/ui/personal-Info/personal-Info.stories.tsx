abfa02f5f9410b3c822b464ee56ae87b2a53155f
eleonora <nebrudko18@gmail.com>
2024-06-21 16:43:31 +0300

    reorganization of code PersonalInfo & added more styles

--- src/components/ui/personal-Info/personal-Info.stories.tsx
+++ src/components/ui/personal-Info/personal-Info.stories.tsx
@@ -1,3 +1,6 @@
+import { ComponentProps } from 'react'
+
+import { action } from '@storybook/addon-actions'
 import { Meta, StoryObj } from '@storybook/react'
 
 import { PersonalInfo } from './personal-Info'
@@ -12,10 +15,27 @@ const meta: Meta<typeof PersonalInfo> = {
 export default meta
 type Story = StoryObj<typeof meta>
 
+type PersonalInfoWrapperProps = ComponentProps<typeof PersonalInfo>
+
+const PersonalInfoWrapper = ({ ...props }: PersonalInfoWrapperProps) => {
+  const avatarClick = action('buttonAvatar was invoked')
+  // const nameClick = action('buttonName was invoked') //for tests
+  // const logOutClick = action('buttonLogOut was invoked')
+
+  return (
+    <div>
+      <div onClick={avatarClick}>
+        <PersonalInfo {...props} />
+      </div>
+    </div>
+  )
+}
+
 export const PersonalInfoBase: Story = {
   args: {
     name: 'Ivan',
     photoDesc: 'name',
     src: './src/assets/webp/avatar-default.webp',
   },
+  render: args => <PersonalInfoWrapper {...args} />,
 }
--- src/components/ui/personal-Info/personal-Info.tsx
+++ src/components/ui/personal-Info/personal-Info.tsx
@@ -1,4 +1,4 @@
-import { LogOutOutline } from '@/assets/components/svgIcons'
+import { Edit2Outline, LogOutOutline } from '@/assets/components/svgIcons'
 import { Avatar } from '@/components/ui/avatar'
 import { Button } from '@/components/ui/button'
 import { Card } from '@/components/ui/card'
@@ -17,8 +17,13 @@ type PersonalInfoProps = {
 export const PersonalInfo = ({ name, photoDesc, src }: PersonalInfoProps) => {
   const cn = {
     avatar: clsx(s.avatar),
+    avatarWrapper: clsx(s.avatarWrapper),
+    buttonEdit2Outline: clsx(s.buttonEdit2Outline),
+    buttonLogOutOutline: clsx(s.buttonLogOutOutline),
+    buttonSecondary: clsx(s.buttonSecondary),
     container: clsx(s.container),
     hint: clsx(s.hint),
+    svg: clsx(s.svg),
   }
 
   return (
@@ -27,16 +32,30 @@ export const PersonalInfo = ({ name, photoDesc, src }: PersonalInfoProps) => {
         <Typography as={'h1'} variant={'h1'}>
           Personal Information
         </Typography>
-        <Avatar className={cn.avatar} size={'l'} src={src} title={photoDesc}></Avatar>
-        <Typography as={'h2'} variant={'h2'}>
-          {name}
-        </Typography>
+
+        <div className={cn.avatarWrapper}>
+          <Avatar className={cn.avatar} size={'l'} src={src} title={photoDesc} />
+          <Button className={cn.buttonSecondary} variant={'secondary'}>
+            <Edit2Outline className={cn.svg} />
+          </Button>
+        </div>
+
+        <FlexContainer jc={'center'}>
+          <Typography as={'h2'} variant={'h2'}>
+            {name}
+          </Typography>
+          <Button className={cn.buttonEdit2Outline} variant={'icon'}>
+            <Edit2Outline className={cn.svg} />
+          </Button>
+        </FlexContainer>
+
         <Typography className={cn.hint} variant={'body2'}>
           j&johnson@gmail.com
         </Typography>
-        <Button variant={'secondary'}>
-          <LogOutOutline />
-          Logout
+
+        <Button className={cn.buttonLogOutOutline} variant={'secondary'}>
+          <LogOutOutline className={cn.svg} />
+          <Typography variant={'subtitle2'}>Logout</Typography>
         </Button>
       </FlexContainer>
     </Card>
