68ad047cb4615f0d0fbd20db57770166943facbf
eleonora <nebrudko18@gmail.com>
2024-06-22 12:32:42 +0300

    fixed UserProfile & Dropdown stories

--- src/shared/ui/header/header.tsx
+++ src/shared/ui/header/header.tsx
@@ -1,14 +1,11 @@
 import { ComponentPropsWithoutRef, ElementRef, forwardRef } from 'react'
 
 import { Card } from '@/components/ui/card'
-import { User } from '@/types'
 import clsx from 'clsx'
 
 import s from './header.module.scss'
 
-type Props = {
-  userData?: User
-} & ComponentPropsWithoutRef<typeof Card>
+type Props = ComponentPropsWithoutRef<typeof Card>
 
 type PropsRef = ElementRef<typeof Card>
 
