f44a745ba618bee6fb6328e8b17792cba1515bac
Ivan Dolgikh <ivan.dolgikh1199@gmail.com>
2024-06-23 12:27:19 +0300

    refactor: deploy issues related to storybook

--- src/components/ui/actions/actions.stories.ts
+++ src/components/ui/actions/actions.stories.ts
@@ -1,24 +1,43 @@
 import { Actions } from '@/components/ui/actions/actions'
+import { VARIANT } from '@/shared/enums/enums'
 import { action } from '@storybook/addon-actions'
 import { Meta, StoryObj } from '@storybook/react'
 
 const meta = {
-  argTypes: {
-    showAll: { control: 'boolean' },
-  },
+  argTypes: {},
   component: Actions,
-  tags: ['autodocs'],
   title: 'Components/Actions',
 } satisfies Meta<typeof Actions>
 
 export default meta
 type Story = StoryObj<typeof meta>
 
-export const CheckEmailExample: Story = {
+export const ActionsAll: Story = {
   args: {
     onDelete: action('delete invoked!'),
     onEdit: action('edit invoked!'),
-    onPlay: action('play invoked!'),
+    onLearn: action('play invoked!'),
     style: { maxWidth: '100%' },
+    variant: VARIANT.ALL,
+  },
+}
+
+export const ActionsLearn: Story = {
+  args: {
+    onDelete: action('delete invoked!'),
+    onEdit: action('edit invoked!'),
+    onLearn: action('play invoked!'),
+    style: { maxWidth: '100%' },
+    variant: VARIANT.ONLY_LEARN,
+  },
+}
+
+export const ActionsEdit: Story = {
+  args: {
+    onDelete: action('delete invoked!'),
+    onEdit: action('edit invoked!'),
+    onLearn: action('play invoked!'),
+    style: { maxWidth: '100%' },
+    variant: VARIANT.ONLY_EDITS,
   },
 }
