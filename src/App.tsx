3c8b10c9cb58fa1f290c59c1c5b953e622e5b43d
M4k4v3ll1 <141486338+M4k4v3ll1@users.noreply.github.com>
2024-06-24 11:41:22 +0400

    fix: vercel deploy issue fixed

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import { SignUpForm } from '@/components/forms'
 import { Grade } from '@/components/ui/grade'
 import { FlexContainer } from '@/shared/ui/flex-container'
 import { Header } from '@/shared/ui/header'
@@ -6,17 +5,11 @@ import { Layout } from '@/shared/ui/layout'
 import { Page } from '@/shared/ui/page'
 
 export function App() {
-  const image = {
-    alt: 'Image',
-    src: 'src/assets/webp/react-logo.webp',
-  }
-
   return (
     <Layout>
       <Header />
       <Page>
         <FlexContainer gap={'10px'} jc={'center'}>
-          <SignUpForm onSubmit={foo} />
           <FlexContainer fd={'column'} gap={'10px'}>
             <Grade />
             <Grade stars={3} />
