4d66a6aa94ebc7b98192ea823c266a8dcec23b95
Ivan Dolgikh <ivan.dolgikh1199@gmail.com>
2024-06-24 12:21:36 +0300

    refactor: PR issues fixed

--- src/components/ui/actions/utils/utils.ts
+++ src/components/ui/actions/utils/utils.ts
@@ -2,6 +2,8 @@ import { ACTIONS, VARIANT } from '@/shared/enums/enums'
 
 import { ActionButton } from '../actions'
 
+// todo: move util function to common according to FSD later.
+// todo: function is expected to be reused in DropDown component.
 export function getActionButtons(tools: ActionButton[], showTools: VARIANT) {
   if (showTools === VARIANT.ONLY_LEARN) {
     return tools.filter(el => el.label === ACTIONS.LEARN)
