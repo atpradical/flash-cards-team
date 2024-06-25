419f5b7b433a8188cd3fc77b9e631bdb193e75f5
eleonora <nebrudko18@gmail.com>
2024-06-25 15:25:18 +0300

    fixed

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,13 +1,11 @@
 import { Router } from '@/app/router'
 import { CardsHeader } from '@/components/ui/cards-header'
-import { ProfilePage } from '@/pages/profile-page'
 import { Layout } from '@/shared/ui/layout'
 
 export function App() {
   return (
     <Layout>
       <CardsHeader isAuthorized />
-      <ProfilePage />
       <Router />
     </Layout>
   )
--- src/pages/profile-page/profile-page.tsx
+++ src/pages/profile-page/profile-page.tsx
@@ -1,16 +1,13 @@
 import { PersonalInfo } from '@/components/ui/personal-info'
 import { FlexContainer } from '@/shared/ui/flex-container'
 import { Page } from '@/shared/ui/page'
+import src from '@/assets/webp/avatar-default.webp'
 
 export const ProfilePage = () => {
   return (
     <Page>
       <FlexContainer jc={'center'}>
-        <PersonalInfo
-          name={'Ivan'}
-          photoDesc={'avatar'}
-          src={'@/assets/webp/avatar-default.webp'}
-        />
+        <PersonalInfo name={'Ivan'} photoDesc={'avatar'} src={src} />
       </FlexContainer>
     </Page>
   )
