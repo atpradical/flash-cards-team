b23038d19dbfe09348fb9f59cec4e39a18edd443
Ivan Dolgikh <ivan.dolgikh1199@gmail.com>
2024-06-26 14:53:11 +0300

    fix: routes in router.tsx

--- src/app/router.tsx
+++ src/app/router.tsx
@@ -1,6 +1,8 @@
 import { RouteObject, RouterProvider, createBrowserRouter } from 'react-router-dom'
 
 import { CheckEmailPage } from '@/pages/check-email-page'
+import { PasswordRecoveryPage } from '@/pages/password-recovery-page'
+import { ResetPassword } from '@/pages/password-reset'
 import { SignInPage } from '@/pages/sign-in-page'
 
 const publicRoutes: RouteObject[] = [
@@ -9,16 +11,16 @@ const publicRoutes: RouteObject[] = [
     path: '/sign-in',
   },
   {
-    element: <CheckEmailPage />,
-    path: '/check-email',
-  },
-  {
     element: <PasswordRecoveryPage />,
     path: '/password-recovery',
   },
   {
     element: <ResetPassword />,
     path: '/reset-password',
+  },
+  {
+    element: <CheckEmailPage />,
+    path: '/check-email',
   },
 ]
 
