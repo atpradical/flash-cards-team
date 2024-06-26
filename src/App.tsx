f4f9d6c96c145c3cd41fed8bd2667143e6a8b551
Ivan Trandin <trandinii@gmail.com>
2024-06-26 10:23:37 +0400

    fix: hooks deleted, infinity animation added

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,13 @@
-import { useEffect, useState } from 'react'
-
 import { Progress } from '@/components/ui/progress'
 import { Layout } from '@/shared/ui/layout'
 
 import { CardsHeader } from './components/ui/cards-header'
 
 export function App() {
-  const [progress, setProgress] = useState(0)
-
-  useEffect(() => {
-    const timer = setTimeout(() => setProgress(100), 500)
-
-    return () => clearTimeout(timer)
-  }, [])
-
   return (
     <Layout>
       <CardsHeader isAuthorized />
-      <Progress value={progress} />
+      <Progress color={'green'} />
     </Layout>
   )
 }
--- src/components/ui/progress/progress.tsx
+++ src/components/ui/progress/progress.tsx
@@ -1,28 +1,26 @@
-import { CSSProperties, ComponentPropsWithoutRef, ElementRef, forwardRef } from 'react'
+import { CSSProperties, ComponentPropsWithoutRef } from 'react'
 
 import * as ProgressPrimitive from '@radix-ui/react-progress'
 import clsx from 'clsx'
 
 import s from './progress.module.scss'
+
 type ProgressProps = {
   color?: CSSProperties['color']
 } & ComponentPropsWithoutRef<typeof ProgressPrimitive.Root>
-type ProgressRef = ElementRef<typeof ProgressPrimitive.Root>
-export const Progress = forwardRef<ProgressRef, ProgressProps>((props, ref) => {
-  const { className, color, value } = props
+
+export const Progress = (props: ProgressProps) => {
+  const { className, color } = props
   const cn = {
     indicator: clsx(s.indicator, className),
     root: clsx(s.root),
   }
 
   return (
-    <ProgressPrimitive.Root className={cn.root} ref={ref} {...props} value={value}>
-      <ProgressPrimitive.Indicator
-        className={cn.indicator}
-        style={{ backgroundColor: color, transform: `translateX(-${100 - (value || 0)}%)` }}
-      />
+    <ProgressPrimitive.Root className={cn.root} {...props}>
+      <ProgressPrimitive.Indicator className={cn.indicator} style={{ backgroundColor: color }} />
     </ProgressPrimitive.Root>
   )
-})
+}
 
 Progress.displayName = ProgressPrimitive.Root.displayName
