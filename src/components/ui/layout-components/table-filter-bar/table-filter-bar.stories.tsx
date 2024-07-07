2ff3177a877cd0cce512bacae51d68f55de97967
eleonora <nebrudko18@gmail.com>
2024-07-07 16:11:18 +0300

    fixed table-filter-bar story

--- src/components/ui/layout-components/table-filter-bar/table-filter-bar.stories.tsx
+++ src/components/ui/layout-components/table-filter-bar/table-filter-bar.stories.tsx
@@ -1,6 +1,6 @@
 import type { Meta, StoryObj } from '@storybook/react'
 
-import { ChangeEvent, useState } from 'react'
+import { useState } from 'react'
 
 import { TableFilterBar } from '@/components/ui/layout-components'
 import { action } from '@storybook/addon-actions'
@@ -17,32 +17,21 @@ type Story = StoryObj<typeof meta>
 
 export const TableFilterBarBase: Story = {
   args: {
+    onSearchChange: action('search callback invoked'),
     onSliderChange: action('slider onValueChange callback invoked'),
     search: '',
-    searchChangeValue: action('search callback invoked'),
     sliderRange: [0, 100],
   },
   render: args => {
     const [sliderRange, setSliderRange] = useState(args.sliderRange)
-    const [search, setSearch] = useState(args.search)
 
     const sliderRangeHandler = (newRange: number[]) => {
       setSliderRange([...newRange])
       args.onSliderChange(newRange)
     }
 
-    const searchChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
-      setSearch(e.currentTarget.value)
-      args.searchChangeValue(e)
-    }
-
     return (
-      <TableFilterBar
-        onSliderChange={sliderRangeHandler}
-        search={search}
-        searchChangeValue={searchChangeHandler}
-        sliderRange={sliderRange}
-      />
+      <TableFilterBar {...args} onSliderChange={sliderRangeHandler} sliderRange={sliderRange} />
     )
   },
 }
