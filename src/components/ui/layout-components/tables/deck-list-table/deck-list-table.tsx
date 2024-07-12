b3991eeb2457add7e59bb4fcc9b5189e7ec917b1
Ivan Dolgikh <ivan.dolgikh1199@gmail.com>
2024-07-12 13:22:05 +0300

    fex: deploy to vercel issues fixed

--- src/components/ui/layout-components/tables/deck-list-table/deck-list-table.tsx
+++ src/components/ui/layout-components/tables/deck-list-table/deck-list-table.tsx
@@ -1,35 +1,44 @@
+import { useState } from 'react'
+import { generatePath } from 'react-router-dom'
+
 import dummyCover from '@/assets/webp/dummy-cover.webp'
+import { DeckDialogForm, DeleteDialogForm } from '@/components/forms'
 import { Actions } from '@/components/ui/layout-components/actions'
 import { TableBody, TableContainer, TableHeader, TableRow } from '@/components/ui/primitives'
 import { Deck } from '@/services/decks/deck.types'
-import { VARIANT } from '@/shared/enums'
+import { DIALOG_ACTION, DIALOG_ENTITY, PATH, VARIANT } from '@/shared/enums'
 import { convertToDDMMYYYY } from '@/shared/utils/convert-date-ddmmyyyy'
 
 import { HeaderCell, PositionCell } from '../container-components'
 
 type DecksListTableProps = {
-  deckList: Deck[]
-  onDelete: () => void
-  onEdit: () => void
-  onLearn: () => void
+  decks: Deck[]
   onSort: () => void
 }
 
-export const DeckListTable = ({
-  deckList,
-  onDelete,
-  onEdit,
-  onLearn,
-  onSort,
-}: DecksListTableProps) => {
+export const DeckListTable = ({ decks, onSort }: DecksListTableProps) => {
+  const [deckId, setDeckId] = useState('')
+  const [showEditDeckDialog, setShowEditDeckDialog] = useState(false)
+  const [showDeleteDeckDialog, setShowDeleteDeckDialog] = useState(false)
   const sortHandler = () => {
     onSort()
   }
 
-  const TableContent = deckList.map(el => {
+  const TableContent = decks.map(el => {
     const cover = el.cover ?? dummyCover
     const cardsCount = el.cardsCount.toString()
     const updated = convertToDDMMYYYY(el.updated)
+    const learnDeckPath = generatePath(PATH.DECK, { deckId: el.id })
+
+    const openEditDeckHandler = (deckId: string) => {
+      setDeckId(deckId)
+      setShowEditDeckDialog(!showEditDeckDialog)
+    }
+
+    const openDeleteDeckHandler = (deckId: string) => {
+      setDeckId(deckId)
+      setShowDeleteDeckDialog(!showDeleteDeckDialog)
+    }
 
     return (
       <TableRow key={el.id}>
@@ -38,7 +47,12 @@ export const DeckListTable = ({
         <PositionCell content={updated} />
         <PositionCell content={el.author.name} />
         <PositionCell>
-          <Actions onDelete={onDelete} onEdit={onEdit} onLearn={onLearn} variant={VARIANT.ALL} />
+          <Actions
+            onDelete={() => openDeleteDeckHandler(el.id)}
+            onEdit={() => openEditDeckHandler(el.id)}
+            onLearn={learnDeckPath}
+            variant={VARIANT.ALL}
+          />
         </PositionCell>
       </TableRow>
     )
@@ -56,6 +70,20 @@ export const DeckListTable = ({
         </TableRow>
       </TableHeader>
       <TableBody>{TableContent}</TableBody>
+      <DeckDialogForm
+        action={DIALOG_ACTION.UPDATE}
+        deckId={deckId}
+        onOpenChange={setShowEditDeckDialog}
+        open={showEditDeckDialog}
+      />
+      <DeleteDialogForm
+        entity={DIALOG_ENTITY.DECK}
+        entityId={deckId}
+        name={'Name Deck'}
+        onOpenChange={setShowDeleteDeckDialog}
+        onSubmit={() => console.log('delete dialog form submit invoked!')}
+        open={showDeleteDeckDialog}
+      />
     </TableContainer>
   )
 }
