6d005beab699ddc81280a3a2bf7f1d07f7cbabdf
eleonora <nebrudko18@gmail.com>
2024-07-13 18:55:57 +0300

    decomposition with label for slider

--- src/components/ui/primitives/slider/slider.tsx
+++ src/components/ui/primitives/slider/slider.tsx
@@ -8,6 +8,7 @@ import clsx from 'clsx'
 import s from './slider.module.scss'
 
 type Props = {
+  label?: string
   onRangeChange: (value: number[]) => void
   range: number[]
 } & ComponentPropsWithoutRef<typeof RadixSlider.Root>
@@ -15,7 +16,7 @@ type Props = {
 type SliderRef = ElementRef<typeof RadixSlider.Root>
 
 export const Slider = forwardRef<SliderRef, Props>(
-  ({ minStepsBetweenThumbs = 1, onRangeChange, range, ...rest }, ref) => {
+  ({ label, minStepsBetweenThumbs = 1, onRangeChange, range, ...rest }, ref) => {
     const cn = {
       outputWrap: clsx(s.outputWrap),
       range: clsx(s.range),
@@ -25,28 +26,31 @@ export const Slider = forwardRef<SliderRef, Props>(
     }
 
     return (
-      <FlexContainer>
-        <Typography className={cn.outputWrap} variant={'body1'}>
-          {range[0]}
-        </Typography>
-        <RadixSlider.Root
-          className={cn.root}
-          minStepsBetweenThumbs={minStepsBetweenThumbs}
-          onValueChange={onRangeChange}
-          ref={ref}
-          step={1}
-          value={range}
-          {...rest}
-        >
-          <RadixSlider.Track className={cn.track}>
-            <RadixSlider.Range className={cn.range} />
-          </RadixSlider.Track>
-          <RadixSlider.Thumb aria-label={'Start Thumb'} className={cn.thumb} />
-          <RadixSlider.Thumb aria-label={'End Thumb'} className={cn.thumb} />
-        </RadixSlider.Root>
-        <Typography className={cn.outputWrap} variant={'body1'}>
-          {range[1]}
-        </Typography>
+      <FlexContainer ai={'flex-start'} fd={'column'}>
+        {label && <Typography as={'label'}>{label}</Typography>}
+        <FlexContainer>
+          <Typography className={cn.outputWrap} variant={'body1'}>
+            {range[0]}
+          </Typography>
+          <RadixSlider.Root
+            className={cn.root}
+            minStepsBetweenThumbs={minStepsBetweenThumbs}
+            onValueChange={onRangeChange}
+            ref={ref}
+            step={1}
+            value={range}
+            {...rest}
+          >
+            <RadixSlider.Track className={cn.track}>
+              <RadixSlider.Range className={cn.range} />
+            </RadixSlider.Track>
+            <RadixSlider.Thumb aria-label={'Start Thumb'} className={cn.thumb} />
+            <RadixSlider.Thumb aria-label={'End Thumb'} className={cn.thumb} />
+          </RadixSlider.Root>
+          <Typography className={cn.outputWrap} variant={'body1'}>
+            {range[1]}
+          </Typography>
+        </FlexContainer>
       </FlexContainer>
     )
   }
