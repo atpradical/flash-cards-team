19b278c710b563e62d5abaec0255ca223f4ab8da
eleonora <nebrudko18@gmail.com>
2024-07-17 13:51:44 +0300

    added: author.name-asc sort by author.name-asc

--- src/pages/deck-list-page/deck-list-page.tsx
+++ src/pages/deck-list-page/deck-list-page.tsx
@@ -5,6 +5,8 @@ import { DeckDialogForm } from '@/components/forms'
 import { DeckListTable, TableFilterBar } from '@/components/ui/layout-components'
 import { Button, Pagination, Typography } from '@/components/ui/primitives'
 import { PaginationModel, useGetDecksQuery, useMeQuery } from '@/services'
+import { ORDER } from '@/shared/enums'
+import { useSearchParamUpdater } from '@/shared/hooks'
 import { FlexContainer } from '@/shared/ui/flex-container'
 import { Page } from '@/shared/ui/page'
 
@@ -12,18 +14,24 @@ export const DeckListPage = () => {
   const [showAddDeckDialog, setShowAddDeckDialog] = useState(false)
 
   const [searchParams] = useSearchParams()
+  const updateSearchParam = useSearchParamUpdater()
 
   const currentPage = Number(searchParams.get('currentPage') ?? 1)
   const itemsPerPage = Number(searchParams.get('itemsPerPage') ?? 10)
   const min = Number(searchParams.get('min') ?? 0)
   const max = Number(searchParams.get('max') ?? 100)
   const search = searchParams.get('search') ?? ''
-  const orderBy = searchParams.get('orderBy') ?? ''
+  const orderBy = searchParams.get('orderBy') ?? undefined
   const tab = searchParams.get('tab') ?? 'allDecks'
 
   const { data: me } = useMeQuery()
   const authorId = tab === 'myDecks' ? me?.id : undefined
   const favoritedBy = tab === 'favorites' ? me?.id : undefined
+  const orderByAuthor = orderBy ? orderBy : ORDER.AUTHOR_NAME_ASC
+
+  if (!searchParams) {
+    updateSearchParam({ currentPage: 1, orderByAuthor })
+  }
 
   const { data, isFetching } = useGetDecksQuery({
     authorId,
@@ -33,9 +41,13 @@ export const DeckListPage = () => {
     maxCardsCount: max,
     minCardsCount: min,
     name: search || undefined,
-    orderBy: orderBy || undefined,
+    orderBy: orderByAuthor,
   })
   const { items: decks = [], pagination = {} as PaginationModel } = data ?? {}
+
+  console.log('orderBy', orderBy)
+  console.log('orderByAuthor', orderByAuthor)
+  console.log('searchParams', searchParams)
 
   return (
     <Page load={isFetching}>
--- src/shared/enums/order.enum.ts
+++ src/shared/enums/order.enum.ts
@@ -1,4 +1,5 @@
 export enum ORDER {
   ASC = 'asc',
+  AUTHOR_NAME_ASC = 'author.name-asc',
   DESC = 'desc',
 }
