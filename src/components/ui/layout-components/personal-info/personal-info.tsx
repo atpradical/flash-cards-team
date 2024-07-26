bca414fd3c75fa72ae2e687a83fc8192e359f6cc
Ivan Dolgikh <ivan.dolgikh1199@gmail.com>
2024-07-26 10:21:01 +0300

    feature: get profile page data

--- src/components/ui/layout-components/personal-info/personal-info.tsx
+++ src/components/ui/layout-components/personal-info/personal-info.tsx
@@ -1,6 +1,6 @@
 import { EditOutline, LogOut } from '@/assets/icons'
 import { Avatar, Button, Card, Typography } from '@/components/ui/primitives'
-import { useLogoutMutation } from '@/services'
+import { User, useLogoutMutation } from '@/services'
 import { SCREEN_SIZE } from '@/shared/enums'
 import { useCurrentScreenWidth } from '@/shared/hooks'
 import { FlexContainer } from '@/shared/ui/flex-container'
@@ -8,12 +8,10 @@ import { FlexContainer } from '@/shared/ui/flex-container'
 import { cn } from './personal-info.styles'
 
 type PersonalInfoProps = {
-  name: string
-  photoDesc?: string
-  src: string
+  userData?: User
 }
 
-export const PersonalInfo = ({ name, photoDesc, src }: PersonalInfoProps) => {
+export const PersonalInfo = ({ userData }: PersonalInfoProps) => {
   const currentScreenWidth = useCurrentScreenWidth()
   const breakpoint = SCREEN_SIZE.MOBILE_TINY
   const isTinyScreen = currentScreenWidth <= breakpoint
@@ -31,26 +29,21 @@ export const PersonalInfo = ({ name, photoDesc, src }: PersonalInfoProps) => {
           Personal Information
         </Typography>
         <FlexContainer className={cn.wrapper}>
-          <Avatar
-            className={cn.avatar}
-            size={isTinyScreen ? 'm' : 'l'}
-            src={src}
-            title={photoDesc}
-          />
+          <Avatar className={cn.avatar} size={isTinyScreen ? 'm' : 'l'} src={userData?.avatar} />
           <Button className={cn.editAvatar} variant={'secondary'}>
             <EditOutline className={cn.icon} />
           </Button>
         </FlexContainer>
         <FlexContainer gap={'12px'} jc={'center'}>
           <Typography as={'h2'} variant={'h2'}>
-            {name}
+            {userData?.name}
           </Typography>
           <Button variant={'icon'}>
             <EditOutline className={cn.icon} />
           </Button>
         </FlexContainer>
         <Typography className={cn.hint} gray>
-          j&johnson@gmail.com
+          {userData?.email}
         </Typography>
         <Button className={cn.bottom} onClick={logoutHandler} variant={'secondary'}>
           <LogOut className={cn.icon} />
