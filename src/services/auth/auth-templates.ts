79228dac0ed2ad159824a911e81b683f0cf81951
Ivan Dolgikh <ivan.dolgikh1199@gmail.com>
2024-07-28 20:14:29 +0300

    feature: pwd-recovery-email

--- src/services/auth/auth-templates.ts
+++ src/services/auth/auth-templates.ts
@@ -1,3 +1,7 @@
-export const emailBodyHTML = `<b>Hello, ##name##!</b><br/>Please confirm your email by clicking on the link below:<br/><a href=\\"http://localhost:5173/confirm-email/##token##\\">Confirm email</a>. If it doesn'\\''t work, copy and paste the following link in your browser:<br/>http://localhost:5173/confirm-email/##token##`
+export const emailConfirmationBodyHTML = `<b>Hello, ##name##!</b><br/>Please confirm your email by clicking on the link below:<br/><a href=\\"http://localhost:5173/confirm-email/##token##\\">Confirm email</a>. If it doesn'\\''t work, copy and paste the following link in your browser:<br/>http://localhost:5173/confirm-email/##token##`
 
-export const emailSubjectHTML = 'Email confirmation request'
+export const emailConfirmationRequestSubjectHTML = 'Email confirmation request'
+
+export const pwdRecoverRequestSubjectHTML = 'Password recovery request'
+
+export const pwdRecoverRequestBodyHTML = `<h1>Hi, ##name##</h1><p>Click <a href=\\"http://localhost:5173/reset-password/##token##\\">here</a> to recover your password</p> If it doesn'\\''t work, copy and paste the following link in your browser:<br/>http://localhost:5173/reset-password/##token##`
