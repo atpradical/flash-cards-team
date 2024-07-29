fd7a1bdca07c4da348a2de325c8f5dd02559bc04
Ivan Dolgikh <ivan.dolgikh1199@gmail.com>
2024-07-29 16:36:39 +0300

    feature: toastify instead of radix toast

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,12 +1,16 @@
 import { Provider } from 'react-redux'
 
 import { Router } from '@/app/router'
+import { Toast } from '@/components/ui/primitives'
 import { store } from '@/services'
+
+import 'react-toastify/dist/ReactToastify.css'
 
 export function App() {
   return (
     <Provider store={store}>
       <Router />
+      <Toast />
     </Provider>
   )
 }
