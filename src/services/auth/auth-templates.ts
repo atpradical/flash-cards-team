8f5ecfc7e91c9afa4b65689e623a79c9015dacf1
Ivan Dolgikh <ivan.dolgikh1199@gmail.com>
2024-08-07 11:36:51 +0300

    upd: html email confirmation template

--- src/services/auth/auth-templates.ts
+++ src/services/auth/auth-templates.ts
@@ -1,7 +1,7 @@
-export const emailConfirmationBodyHTML = `<b>Hi, ##name##!</b><br/>Please confirm your email by clicking on the link below:<br/><a href="https://flash-cards-team-atpradicals-projects.vercel.app/confirm-email/##token##">Confirm email</a>. If it doesn't work, copy and paste the following link in your browser:<br/>https://flash-cards-team-atpradicals-projects.vercel.app/confirm-email/##token##`
+export const emailConfirmationBodyHTML = `<b>Hello, ##name##!</b><br/>Please confirm your email by clicking on the link below:<br/><a href="https://flash-cards-team-atpradicals-projects.vercel.app/confirm-email/##token##">Confirm email</a>. If it doesn't work, copy and paste the following link in your browser:<br/>https://flash-cards-team-atpradicals-projects.vercel.app/confirm-email/##token##`
 
 export const emailConfirmationRequestSubjectHTML = 'Flashcards-app: email confirmation request'
 
 export const pwdRecoverRequestSubjectHTML = 'Flashcards-app: password recovery request'
 
-export const pwdRecoverRequestBodyHTML = `<h1>Hi, ##name##</h1><p>Click <a href="https://flash-cards-team-atpradicals-projects.vercel.app/reset-password/##token##">here</a> to recover your password</p> If it doesn't work, copy and paste the following link in your browser:<br/>https://flash-cards-team-atpradicals-projects.vercel.app/reset-password/##token##`
+export const pwdRecoverRequestBodyHTML = `<h1>Hello, ##name##</h1><p>Click <a href="https://flash-cards-team-atpradicals-projects.vercel.app/reset-password/##token##">here</a> to recover your password</p> If it doesn't work, copy and paste the following link in your browser:<br/>https://flash-cards-team-atpradicals-projects.vercel.app/reset-password/##token##`
